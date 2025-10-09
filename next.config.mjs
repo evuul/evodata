af20d259677d8aa0e40385b56c5ae5dce187b2a1
alexanderek <Alexander.ek@live.se>
2025-10-09 11:44:38 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  },
+  outputFileTracingIncludes: {
+    "/api/casinoscores/players/[game]": ["./node_modules/@sparticuz/chromium/bin/**"],
+    "/api/casinoscores/players/[game]/route": ["./node_modules/@sparticuz/chromium/bin/**"],
+  },
+};
 
 export default nextConfig;
